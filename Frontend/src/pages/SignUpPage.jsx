3c0715a7cd5741c349acc241403721fdbb3fb04a
Ridima Shrotriya <ridimashrotriya27@gmail.com>
2025-07-18 15:22:20 +0530

    completed

--- Frontend/src/pages/SignUpPage.jsx
+++ Frontend/src/pages/SignUpPage.jsx
@@ -65,7 +65,7 @@ const SignUpPage = () => {
                 <input
                   type="text"
                   className={`input input-bordered w-full pl-10`}
-                  placeholder="eg:  Himanshu Raj"
+                  placeholder="eg:  Ridima Shrotriya"
                   value={formData.fullName}
                   onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
                 />
